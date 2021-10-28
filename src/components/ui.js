1cbdf4293da1a7d306cf151649d99881d642da0a
Ajay Jarhad <ajayjarhad1@gmail.com>
2021-10-28 17:23:14 +0530

    feat(ui): hide on smaller screen

--- src/components/ui.js
+++ src/components/ui.js
@@ -7,7 +7,7 @@ const UI = () => {
     <>
       <Header />
       <div className="min-h-screen flex flex-row ">
-        <div className="flex flex-col bg-blue-500 text-white w-96">
+        <div className="flex flex-col bg-blue-500 text-white w-96 mobile:hidden">
           <ul className="flex flex-col py-4 sticky top-0">
             <Sidebar />
           </ul>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    screens: {
+      mobile: { max: "639px" },
+    },
   },
   variants: {
     extend: {},
